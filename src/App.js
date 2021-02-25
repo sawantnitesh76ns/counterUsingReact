1dd8adf9edbad613147a5937e3f55b7ef42b7cf8
Nitesh <sawantnitesh76.ns@gmail.com>
2021-02-25 08:41:31 +0530

    Adding Counter project

--- src/App.js
+++ src/App.js
@@ -1,25 +1,22 @@
-import logo from './logo.svg';
+import React,{useState} from "react";
 import './App.css';
 
+//Importing Compoenents
+import Form from "./Components/form.js";
+import TodoList from "./Components/TodoList.js";
+
 function App() {
+  const[inputText,setInputTex]=useState("");
+  const[todos,setTodos]=useState([]);
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+      <header>
+        <h1>{inputText}</h1>
       </header>
+      <Form todos={todos} setTodos={setTodos} setInputText={setInputTex} inputText={inputText}/>
+      <TodoList />
     </div>
   );
-}
+};
 
 export default App;
